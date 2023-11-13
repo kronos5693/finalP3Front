fbd0e997094d4e19cfadaf0924aa582057acfba6
kronos5693 <krull.w@gmail.com>
2023-11-13 17:44:27 -0300

    add creacion de usuario

--- src/components/SingUp/SingUp.jsx
+++ src/components/SingUp/SingUp.jsx
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+import axios from 'axios';
 
-const SingUp = () => {
+const apiUrl="http://localhost:3000/usuarios"
+
+const SignUp = () => {
   const [formData, setFormData] = useState({
     firstName: '',
     lastName: '',
     phoneNumber: '',
     email: '',
+    password: '',
+    confirmPassword: '',  
   });
 
   const handleChange = (e) => {
@@ -18,11 +23,43 @@ const SingUp = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  
+
     console.log('Nombre: ', formData.firstName);
     console.log('Apellido: ', formData.lastName);
     console.log('Teléfono: ', formData.phoneNumber);
     console.log('Correo Electrónico: ', formData.email);
+    console.log('Contraseña: ', formData.password);
+    console.log('Repetir Contraseña: ', formData.confirmPassword);
+
+  
+    if (formData.password !== formData.confirmPassword) {
+      alert('Las contraseñas no coinciden');
+      return;
+    }
+
+    const dataToSend = {
+      nombre: formData.firstName,
+      apellido: formData.lastName,
+      contraseña: formData.password,
+      //telefono:formData.telefono,
+      email: formData.email,
+      roles: "cliente",
+    };
+
+    try {
+      // Enviar los datos a la API usando Axios
+      const response =  axios.post(apiUrl, dataToSend);
+
+      console.log('Datos enviados correctamente a la API', response.data);
+    } catch (error) {
+      console.error('Error al enviar datos a la API', error);
+    }
+
+
+
+
+
+    
   };
 
   return (
@@ -61,6 +98,25 @@ const SingUp = () => {
           fullWidth
           margin="normal"
         />
+        <TextField
+          name="password"
+          label="Contraseña"
+          type="password"
+          value={formData.password}
+          onChange={handleChange}
+          fullWidth
+          margin="normal"
+        />
+      
+        <TextField
+          name="confirmPassword"
+          label="Repetir Contraseña"
+          type="password"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          fullWidth
+          margin="normal"
+        />
         <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '10px' }}>
           <Button variant="contained" color="primary" type="submit">
             Registrarse
@@ -71,4 +127,4 @@ const SingUp = () => {
   );
 };
 
-export default SingUp;
+export default SignUp;
