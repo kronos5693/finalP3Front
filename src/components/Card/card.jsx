df2892c631ac1d1f83375ae3936ab5cfe832e33c
Werner Krull <w.krull@hotmail.com>
2026-02-22 16:09:29 -0300

    fix seccion

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -5,6 +5,7 @@ import Grid from "@mui/material/Grid";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import CardMedia from "@mui/material/CardMedia";
+import Box from "@mui/material/Box";
 import { Button, CardActionArea } from "@mui/material";
 import { Link } from "react-router-dom";
 import api from '../../services/api';
@@ -76,59 +77,162 @@ function Tarjeta({ bandera }) {
     );
   }
 
-  let randomItems = [];
-  if (bandera) {
-    randomItems = getRandomItems(post, 3);
-  } else {
-    randomItems = getRandomItems(post, 6);
-  }
+  let randomItems = getRandomItems(post, 6);
 
   return (
-    <Container maxWidth="lg">
-      <Card>
-        <CardContent>
-          <Typography variant="h4" align="center">
-            LOS MAS VISITADOS
-          </Typography>
-          <Grid container spacing={5} style={{ marginTop: "25px" }}>
-            {randomItems.map((point, index) => (
-              <Grid item xs={12} sm={4} key={index}>
-                {point && point.img && point.excursion && point.descripcion && point.precio && (
-                  <Card sx={{ maxWidth: 345 }} style={{ padding: "10px", marginTop: "30px", display: "flex", flexDirection: "column" }}>
-                    <CardActionArea>
-                      <CardMedia
-                        component="img"
-                        height="140"
-                        image={point.img}
-                        alt={point.excursion}
-                        style={{ borderRadius: "5px" }}
-                      />
-                      <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                          {point.excursion}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary" align="justify">
-                          {point.descripcion}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                          Precio: ${point.precio}
-                        </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                    <div style={{ display: "flex" }}>
-                      <Link to={`/reserva/${point.excursion}`} style={{ width: "100%" }}>
-                        <Button variant="contained" size="small" style={{ width: "100%" }}>
-                          RESERVAR
-                        </Button>
-                      </Link>
-                    </div>
-                  </Card>
-                )}
-              </Grid>
-            ))}
+    <Container 
+      maxWidth={false}
+      sx={{ 
+        maxWidth: "1400px",
+        margin: "0 auto",
+        paddingX: { xs: 2, sm: 3, md: 4 },
+        paddingY: 4 
+      }}
+    >
+      <Typography 
+        variant="h4" 
+        align="center" 
+        sx={{ 
+          marginBottom: 4, 
+          fontWeight: 700,
+          color: "#1976d2"
+        }}
+      >
+        LOS MÁS VISITADOS
+      </Typography>
+      <Grid 
+        container 
+        spacing={{ xs: 2, sm: 2.5, md: 3 }}
+        sx={{
+          justifyContent: "center"
+        }}
+      >
+        {randomItems.map((point, index) => (
+          <Grid 
+            item 
+            xs={12} 
+            sm={6} 
+            md={4} 
+            key={index}
+            sx={{
+              display: "flex",
+              justifyContent: "center"
+            }}
+          >
+            {point && point.img && point.excursion && point.descripcion && (
+              <Card 
+                elevation={2}
+                sx={{ 
+                  width: "100%",
+                  maxWidth: "400px",
+                  height: "100%",
+                  display: "flex", 
+                  flexDirection: "column",
+                  transition: "all 0.3s ease-in-out",
+                  "&:hover": {
+                    transform: "translateY(-8px)",
+                    boxShadow: "0 12px 20px rgba(0,0,0,0.15)"
+                  }
+                }}
+              >
+                <CardActionArea 
+                  component={Link} 
+                  to={`/reserva/${point.excursion}`}
+                  sx={{ 
+                    flexGrow: 1, 
+                    display: "flex", 
+                    flexDirection: "column", 
+                    alignItems: "stretch"
+                  }}
+                >
+                  <Box
+                    sx={{
+                      width: "100%",
+                      height: "220px",
+                      overflow: "hidden",
+                      position: "relative",
+                      backgroundColor: "#f5f5f5"
+                    }}
+                  >
+                    <CardMedia
+                      component="img"
+                      image={point.img}
+                      alt={point.excursion}
+                      sx={{ 
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                        objectPosition: "center"
+                      }}
+                    />
+                  </Box>
+                  <CardContent 
+                    sx={{ 
+                      flexGrow: 1, 
+                      padding: 2,
+                      display: "flex",
+                      flexDirection: "column"
+                    }}
+                  >
+                    <Typography 
+                      gutterBottom 
+                      variant="h6" 
+                      component="h3"
+                      sx={{
+                        fontSize: "1.15rem",
+                        fontWeight: 600,
+                        marginBottom: 1,
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        display: "-webkit-box",
+                        WebkitLineClamp: 2,
+                        WebkitBoxOrient: "vertical",
+                        minHeight: "2.6em",
+                        lineHeight: 1.3
+                      }}
+                    >
+                      {point.excursion}
+                    </Typography>
+                    <Typography 
+                      variant="body2" 
+                      color="text.secondary"
+                      sx={{
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        display: "-webkit-box",
+                        WebkitLineClamp: 3,
+                        WebkitBoxOrient: "vertical",
+                        fontSize: "0.9rem",
+                        lineHeight: 1.5
+                      }}
+                    >
+                      {point.descripcion}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <Box sx={{ padding: 2, paddingTop: 0 }}>
+                  <Link to={`/reserva/${point.excursion}`} style={{ textDecoration: "none" }}>
+                    <Button 
+                      variant="contained" 
+                      fullWidth
+                      size="large"
+                      sx={{
+                        textTransform: "none",
+                        fontWeight: 600,
+                        fontSize: "1rem",
+                        padding: "10px 24px",
+                        borderRadius: "8px"
+                      }}
+                    >
+                      Reservar
+                    </Button>
+                  </Link>
+                </Box>
+              </Card>
+            )}
           </Grid>
-        </CardContent>
-      </Card>
+        ))}
+      </Grid>
     </Container>
   );
 }
